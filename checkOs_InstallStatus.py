4f36497f4cb798773811ee50a1838703a2adbdc6
zjizhi <zjizhi@qq.com>
2014-08-28 21:37:19 +0800

    remove the multi-thread in reboot and add some error report

--- checkOs_InstallStatus.py
+++ checkOs_InstallStatus.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python
 
+## ################################################################################
+## the package and lib that must install:
+##
+## OpenIPMI
+##  yum install OpenIPMI-python
+##
+## Pexpect:Version 3.3 or higher
+##  caution: a lower version will cause some error like "timeout nonblocking() in read" when you log to a host by ssh
+##      wget https://pypi.python.org/packages/source/p/pexpect/pexpect-3.3.tar.gz
+##      tar xvf pexpect-3.3.tar.gz
+##      cd pexpect-3.3
+##      python setup install
+##
+##        
+## Be aware:
+##    2014-08-24 : using multiprocessing.dummy to archieve multi-thread instead of multi-processing with multiprocessing
+##         in multi-process, the function pssh will cause error like "local variable 's' referenced before assignment"
+##      
+## ################################################################################
+
 import os
 import sys
 import pexpect
@@ -19,46 +39,61 @@ def pssh((hostname,username,password,cli)):
         # print 'except'
         output=s.before
     except Exception,e:
-        print '\nException Occur',str(e)
+        print '\nException Occur in ssh to host %s ,Error is:\n %s' % (hostname, str(e))
     finally:
-        s.logout()
 	s.close()
     return [hostname,output]
 
 def pxe((hostname,commandList)):
     print "pxe %s" % hostname
-    result = []
+    result = 0
     for command in commandList :
         print 'pxe command:%s' % command
-        result.append(subprocess.call(command.split(" ")))
+        res=subprocess.call(command.split(" "))
+
+        if res == 1:
+            result = 1
+            print 'pxe error in host %s' % hostname
+            break
+
     return [hostname, result]
 
-def rebootAndInstall(processCnt,hostLists):
-    from multiprocessing import Pool
-    pool = Pool(processes=processCnt)
-    print hostLists
-    ThreadUnits=[]
-    for hostname in hostLists :
-        commandList = []
-        commandList.append("ipmitool -l lanplus -H %s -U admin -P admin chassis power status" % (hostname))
-        # commandList.append("ipmitool -I lanplus -H %s -U admin -P admin power reset" % (hostname))
-        ThreadUnits.append((hostname,commandList))
+def rebootAndInstall(_hostList):
+     TimeInterval=5
+     RebootHostInPerInterval=2
+    
+     with open('restartError.log','w') as file:
+         file.truncate()
 
-    if ThreadUnits :
-        res = pool.map_async(pxe,ThreadUnits)
-        result = res.get()
-    if  host in result :
-        print 'reboot status:%s' % result[1][0]
-    pool.close()
-    pool.join()
+     while True:
+         for i in range(1,RebootHostInPerInterval+1) :
+             if _hostList :
+                 commandList = []
+                 commandList.append("ipmitool -l lanplus -H %s -U admin -P admin chassis power status" % (_hostList[0]))
+                 commandList.append("ipmitool -I lanplus -H %s -U admin -P admin power reset" % (_hostList[0]))
+                 result = pxe((_hostList[0],commandList))
+                 
+                 if result[1] == 1:
+                     with open('restartError.log','a') as file:
+                         file.write(result[0]+'\n')
+                 
+                 #print 'host :%s ,restart state: %s' % (result[0],result[1])
+                 del _hostList[0]
+
+         if _hostList:
+             time.sleep(TimeInterval)
+         else:
+             break
+
+
 
 if __name__ == '__main__':
     multiProcessCount = 2 
     hostList=[]
     hostOsTimeList = []
-    
+    NewOSFilterInterval = 1 #days
     # add the host waiting for check in hostList 
-    hostList.append('10.1.4.22')
+    hostList.append('10.1.4.100')
     hostList.append('10.1.4.23')
 #    print hostList
 
@@ -75,32 +110,37 @@ if __name__ == '__main__':
     import datetime
     import string
     for output in result:
-        timeArr=output[1].split('\n')[1].split(' ')
-        realTimeStruct = time.strptime(timeArr[0]+' '+timeArr[1].split('.')[0],'%Y-%m-%d %H:%M:%S')
-        realTime = datetime.datetime(*realTimeStruct[:6])
-        osInstallTime_UTC = None
-        utcDelta=string.atoi(timeArr[2][1:])
-        if '+' in timeArr[2]:
-            osInstallTime_UTC = realTime + datetime.timedelta(hours=-1*(utcDelta/100))
-        elif '-' in timeArr[2]:
-            osInstallTime_UTC = realTime + datetime.timedelta(hours=1*(utcDelta/100))
-       
-        hostOsTimeList.append((output[0],osInstallTime_UTC))
-    
+        if output[1] and output[1] != '' :
+            timeArr=output[1].split('\n')[1].split(' ')
+            realTimeStruct = time.strptime(timeArr[0]+' '+timeArr[1].split('.')[0],'%Y-%m-%d %H:%M:%S')
+            realTime = datetime.datetime(*realTimeStruct[:6])
+            osInstallTime_UTC = None
+            utcDelta=string.atoi(timeArr[2][1:])
+            if '+' in timeArr[2]:
+                osInstallTime_UTC = realTime + datetime.timedelta(hours=-1*(utcDelta/100))
+            elif '-' in timeArr[2]:
+                osInstallTime_UTC = realTime + datetime.timedelta(hours=1*(utcDelta/100))
+            
+            hostOsTimeList.append((output[0],osInstallTime_UTC))
+        else:
+            print 'Host %s connection failed' % output[0]
+
     curTime = datetime.datetime.utcnow()
     print 'current Utc Time :%s' % curTime
-    print hostOsTimeList    
+    # print hostOsTimeList
+    
     oldOsHost = []
     for host in hostOsTimeList :
-        if  (curTime - host[1]).days >1 :
-            # print 'host %s \'OS is not a fresh one' % host[0]
+        print (curTime - host[1]).days
+        if  (curTime - host[1]).days > NewOSFilterInterval :
+            print 'host %s \'OS is not a fresh one' % host[0]
             oldOsHost.append(host[0])
     if oldOsHost :
+        print 'These Hosts\' Os are not reinstall: \n'
         print oldOsHost 
     
-    reboot = ['10.0.0.10']
-    
-    rebootAndInstall(2,reboot)
+    reboot = oldOsHost    
+    rebootAndInstall(reboot)
     
     pool.close()
     pool.join()    
